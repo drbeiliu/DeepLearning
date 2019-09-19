0737571c51b261cdc24132e43e651c0cfc443301
B D <dongbowei0526@gmail.com>
2019-09-19 12:37:30 -0400

    Add files via upload

--- test_models/test_all.py
+++ test_models/test_all.py
@@ -14,9 +14,19 @@ import warnings
 warnings.filterwarnings('ignore')
 
 model_dict = {
-    '''name:[".pkl name(not adding date)", data_in_normalization, data_out_normalization, train_in_size, mix_in_image(AVG or MAX)]'''
+    "HE_HER_microtubule_microtubule":["HE_X2_HER",5315.0,15383.0,15,None],
+    "HE_HER_microtubule_adhesion_1":["HE_X2_HER",5542.0,8029.0,15,None],
+    "HE_HER_microtubule_adhesion_0":["HE_X2_HER",5315.0,15383.0,15,None],
+    "HE_HER_microtubule_factin_0":["HE_X2_HER",5315.0,15383.0,15,None],
+    "HE_HER_microtubule_factin_1":["HE_X2_HER",9680.0,15383.0,15,None],
+    "HE_HER_microtubule_mitotracker_0":["HE_X2_HER",5315.0,15383.0,15,None],
+    "HE_HER_microtubule_mitotracker_1":["HE_X2_HER",8345.0,15383.0,15,None],
+    "HE_HER_adhesion_adhesion":["HE_X2_HER",5542.0,8029.0,15,None],
+    "HE_HER_mitotracker_mitotracker":["HE_X2_HER",5315.0,15383.0,15,None],
+    "HE_HER_factin_factin":["HE_X2_HER",5315.0,15383.0,15,None],
     "LE_HE":["LE_HE",206.0,8029.0,15,None],
     "LE_HE_enlarge":["LE_X2_HE_X2",190.0,8029.0,15,None],
+    "HE_HER_199":["LE_X2_HE_X2", 8130.0, 15383.0,15,None],
     "LE_HER":["LE_X2_HER",190.0,8029.0,15,None],
     "HE_HER": ["HE_X2_HER",5542.0,8029.0,15,None],
     "HE_3_HER":["HE_X2_3f_HER",5542.0,8029.0,3,None],
@@ -77,6 +87,7 @@ class ReconsDataset(torch.utils.data.Dataset):
 def modelPredictHER(state_dict_path,dataloader,out_norm,n_channels,n_classes=1):
     # pylint: disable=E1101
     cuda = torch.device('cuda:0')
+    print(cuda)
     # pylint: enable=E1101
     model = UNet(n_channels, n_classes)
 
@@ -97,53 +108,59 @@ def modelPredictHER(state_dict_path,dataloader,out_norm,n_channels,n_classes=1):
         image = image.cuda(cuda)  
         
         pred = model(image)
-        pred = pred*8029.0
-
+        #pred = pred*15383.0
+        pred = pred*out_norm
         results[image_name[0]] = pred.detach().cpu().numpy().astype(np.uint32)
     return results
 
-def loadDatasetDict(dir_path):
+def loadDatasetDict(dir_path,enlarged):
     dataset_dict={}
     for samples in os.listdir(dir_path):
         sample_path = os.path.join(dir_path, samples)
+        if samples not in dataset_dict:
+            dataset_dict[samples] =[]
         if os.path.isdir(sample_path):
             names = os.listdir(sample_path)
-            if samples not in dataset_dict:
-                dataset_dict[samples] =[]
             for name in names:
-                dataset_dict[samples].append(imgread(os.path.join(sample_path,name)))
+                img = imgread(os.path.join(sample_path,name))
+                if enlarged:
+                    if img.shape[0] == 256:
+                        dataset_dict[samples].append(img)
+                    elif img.shape[0] == 128:
+                        img = transform.resize(img,(img.shape[0]*2, img.shape[1]*2),preserve_range=True,order=3)
+                        dataset_dict[samples].append(img)
+                else:
+                    if img.shape[0] == 128:
+                        dataset_dict[samples].append(img)
+    print("length of dataset:", len(dataset_dict))
     return dataset_dict
 
 def imgread(file):
     return np.array(Image.open(file))
 
-def HERtest(name, afterLEPred,batch_size=1):
-    
+def HERtest(state_dict_path,input_dir_path,name, in_norm,out_norm,train_in_size=15,mix_in=None,afterLEPred=False,le_dict=None,batch_size=1):
+  
 
-    if name not in model_dict:
-        print("model name not found! Please use following: ", model_dict.keys())
-    else:
-        stat = model_dict.get(name) #normalization=stat[1], train_in_size=stat[3],mix_in=stat[4]
-        if name == "LE_HER":
-            input_path = os.path.join(os.getcwd(),"dataset","LE_X2")
-            dataset_dict = loadDatasetDict(input_path)
+        if afterLEPred:
+            dataset_dict = le_dict 
         else:
-            if afterLEPred:
-                dataset_dict = LE_HEtest(enlarged=True,batch_size=batch_size) 
-            else:
-                input_path = os.path.join(os.getcwd(),"dataset","HE_X2")
-                dataset_dict = loadDatasetDict(input_path)
+        	dataset_dict = loadDatasetDict(input_dir_path,enlarged=True)
+                
+        print("loading dataset....")
         SRRFDATASET = ReconsDataset(
             img_dict = dataset_dict,
             transform=ToTensor(),
-            normalization=stat[1],
+            normalization=in_norm,
             in_size = 256,
-            train_in_size=stat[3],
-            mix_in=stat[4]
+            train_in_size=train_in_size,
+            mix_in=mix_in
         )
         test_dataloader = torch.utils.data.DataLoader(SRRFDATASET, batch_size=batch_size,shuffle=True,pin_memory=True)
-        stat_dict_path = os.path.join(os.getcwd(),"models",stat[0]+"_0825.pkl")
-        results = modelPredictHER(stat_dict_path,test_dataloader,stat[2],stat[3])
+        print("finished loading dataset...")
+
+        print("start predicting...")
+        results = modelPredictHER(state_dict_path,test_dataloader,out_norm = out_norm,n_channels=train_in_size)
+        print("finished predicting...")
         if afterLEPred:
             save_path = os.path.join(os.getcwd(),"predictions",name+"_adterLEPred")
         else:
@@ -155,41 +172,27 @@ def HERtest(name, afterLEPred,batch_size=1):
             io.imsave(os.path.join(save_path,name+'_pred.tif'),image)
 
 
-def LE_HEtest(enlarged,batch_size=1):
+def LE_HEtest(state_dict_path,input_dir_path, name,in_norm,out_norm,enlarged,batch_size=1):
     if enlarged:
-        dir_path = os.path.join(os.getcwd(),"dataset","LE_X2")
-        state_dict_path = os.path.join(os.getcwd(),"models",model_dict["LE_HE_enlarge"][0]+"_0825.pkl")
         save_path = os.path.join(os.getcwd(),"predictions","LE_HE","pred_X2")
-        in_norm = model_dict["LE_HE_enlarge"][1]
-        out_norm =  model_dict["LE_HE_enlarge"][2]
         in_size = 256
     else:
-        dir_path = os.path.join(os.getcwd(),"dataset","LE")
-        state_dict_path = os.path.join(os.getcwd(),"models",model_dict["LE_HE"][0]+"_0825.pkl")
         save_path = os.path.join(os.getcwd(),"predictions","LE_HE","pred")
-        in_norm = model_dict["LE_HE"][1]
-        out_norm =  model_dict["LE_HE"][2]
         in_size = 128
-    data_dic = loadDatasetDict(dir_path)
+    data_dic = loadDatasetDict(input_dir_path,enlarged=enlarged)
     SRRFDATASET = ReconsDataset(
         img_dict = data_dic,
         transform=ToTensor(),
         normalization=in_norm,
         in_size = in_size
-
     )
     test_dataloader = torch.utils.data.DataLoader(SRRFDATASET, batch_size=batch_size,shuffle=True,pin_memory=True)
-    # pylint: disable=E1101
     cuda = torch.device('cuda:0')
-    # pylint: enable=E1101
     model = UNet(n_channels=15, n_classes=15)
-
     print("{} paramerters in total".format(sum(x.numel() for x in model.parameters())))
     model.cuda(cuda)
     model.load_state_dict(torch.load(state_dict_path))
     model.eval()
-
-    result_dict = {}
     
     for batch_idx, items in enumerate(test_dataloader):
         image = items['image_in']
@@ -213,10 +216,7 @@ def LE_HEtest(enlarged,batch_size=1):
             result_dict[filepath]=[]
         
         for image_num in range(train_in_size):
-            image = pred[0,image_num]*out_norm
-#            mv = image.flatten().min()
-#            if mv < 0:
-#                image = image + abs(mv)
+            image = pred[0,image_num]*out_norm 
             image_name = os.path.join(sample_path, "HE_{:02}.tif".format(image_num))
             result_dict[filepath].append(np.reshape(image.detach().cpu().numpy(),(in_size,in_size)))
             io.imsave(image_name,image.detach().cpu().numpy().astype(np.uint32))
